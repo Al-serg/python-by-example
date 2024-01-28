63963a0bbefac9c0f3bbc17da3924b3aa8b0d747
Al-serg <molodcovaalena74@gmail.com>
2024-01-29 02:03:24 +0300

    Задачи 7-11

---
+++ задача-8.py
@@ -0,0 +1,5 @@
+# Задача 8 
+lunch = int(input("Введите общую сумму обеда: "))
+people = int(input("Введите количество участников обеда: "))
+score = lunch/ people
+print("С каждого по:", score)
---
+++ задача-9.py
@@ -0,0 +1,6 @@
+# Задача 9
+day = int(input("Введите количество часов: "))
+clock = day * 24
+men = clock * 60
+sec = men * 60
+print("Часов:", clock, "Минут:", men, "Секунд:", sec)
