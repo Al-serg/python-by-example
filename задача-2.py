2dd8faa9c1afa3e1215914bd26496487e80c0b8f
Al-serg <molodcovaalena74@gmail.com>
2024-01-29 02:06:10 +0300

    Задачи с 1-6 разбиты по файлам

---
+++ задача-2.py
@@ -0,0 +1,4 @@
+#Задача 2
+name=input("Введите ваше имя")
+surname=input("Введите вашу фамилию")
+print("Привет",name,surname)
---
+++ задача-5.py
@@ -0,0 +1,6 @@
+#Задача 5
+num1=int(input(" Введите первое число"))
+num2=int(input("Введите второе число"))
+num3=int(input("Введите третье число"))
+summ=int(num1+num2)
+print("Сумма чисел:", summ*num3)
